ca03e832d706d296ee5e50b4d6d0605ceafd06b3
afoninartem <47443887+afoninartem@users.noreply.github.com>
2021-01-22 17:41:48 +0300

    carousel and imports

--- script.js
+++ script.js
@@ -1,22 +1,50 @@
-const depts = {
-  warehouse: ["Акции", "Комус", "Визитки", "Поступления", "Отгрузки"],
-  maintenance: ["Обеды"],
-  personal: ["УРВ"],
-};
+//menu buttons
+document.querySelectorAll(".drop__item-btn").forEach((btn) => {
+  btn.addEventListener("click", () => {
+    document.querySelector("head").innerHTML +=
+      '<link rel="import" href="http://artemafonin.ru/handlers/dinners/index.html">';
+    const modulePage = document.querySelector('link[rel="import"]').import;
+    const content = modulePage.querySelector('wrapper');
 
-const dropDownMenu = document.createElement("ul");
+    document.body.appendChild(content.cloneNode(true));
+  });
+});
 
-document.querySelectorAll(".header__nav-btn").forEach((btn) => {
-  if (dropDownMenu.innerHTML === "") {
-    btn.addEventListener("click", () => {
-      depts[btn.value].forEach((elem) => {
-        const li = document.createElement("li");
-        li.innerHTML = `<button type="button" class="drop-btn">${elem}</button>`;
-        dropDownMenu.appendChild(li);
-      });
-    });
-  } else {
-    dropDownMenu.innerHTML = "";
+//carousel
+const carouselSlide = document.querySelector(".carousel-slide");
+const carouselImages = document.querySelectorAll(".carousel-slide img");
+
+const prevBtn = document.querySelector("#prevBtn");
+const nextBtn = document.querySelector("#nextBtn");
+
+let counter = 1;
+const size = carouselImages[0].clientWidth;
+
+carouselSlide.style.transform = `translateX(${-size * counter}px)`;
+
+nextBtn.addEventListener("click", () => {
+  if (counter >= carouselImages.length - 1) return;
+  carouselSlide.style.transition = "transform 0.4s ease-in-out";
+  counter += 1;
+  carouselSlide.style.transform = `translateX(${-size * counter}px)`;
+});
+
+prevBtn.addEventListener("click", () => {
+  if (counter <= 0) return;
+  carouselSlide.style.transition = "transform 0.4s ease-in-out";
+  counter -= 1;
+  carouselSlide.style.transform = `translateX(${-size * counter}px)`;
+});
+
+carouselSlide.addEventListener("transitionend", () => {
+  if (carouselImages[counter].id === "lastClone") {
+    carouselSlide.style.transition = "none";
+    counter = carouselImages.length - 2;
+    carouselSlide.style.transform = `translateX(${-size * counter}px)`;
   }
-  document.querySelector('.header')
+  if (carouselImages[counter].id === "firstClone") {
+    carouselSlide.style.transition = "none";
+    counter = carouselImages.length - counter;
+    carouselSlide.style.transform = `translateX(${-size * counter}px)`;
+  }
 });
